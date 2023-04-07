b9004e9e3e027bc3902e5a7e53e9ee9bc9bb166c
niwei822 <cecily.li.822@gmail.com>
2023-04-06 22:48:43 -0700

    add maintabbarviewcontroller and controllers

---
+++ Netflix Clone Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone Project
+//
+//  Created by cecily li on 4/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
